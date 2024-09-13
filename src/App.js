94af168f76fd7389a5a83c01e5097a5b90f55be6
Juan David <juandavidesme@gmail.com>
2024-09-12 20:32:32 -0500

    Primer commit de preentrega

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
+  const nombre = <h1>Carpi2</h1>
+  const edad = <p>27</p>;
+
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to blow.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {nombre}
+      {nombre}
+      {nombre}
+      {nombre}
+      {nombre}
     </div>
   );
 }
